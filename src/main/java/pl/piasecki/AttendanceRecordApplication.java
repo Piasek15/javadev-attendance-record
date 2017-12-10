3eaf1b8b66abe41f6b75a8f1991a2e23c6cc77ba
Patrick <piasecki.patrick.15@gmail.com>
2017-12-10 11:46:30 +0100

    builded project structure

---
+++ src/main/java/pl/piasecki/AttendanceRecordApplication.java
@@ -0,0 +1,12 @@
+package pl.piasecki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceRecordApplication.class, args);
+	}
+}
